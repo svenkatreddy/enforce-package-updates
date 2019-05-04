af9f8952132ef02fdaf53a070ef0cf248f0f852e
svenkatreddy <svenkatreddy@users.noreply.github.com>
2019-05-04 00:00:57 +0000

    Add config, options to only include pacakages based on name

---
+++ index.js
@@ -0,0 +1,23 @@
+#! /usr/bin/env node
+
+const path = require("path");
+const cwd = process.cwd();
+const fs = require("fs");
+const perf = require("execution-time")();
+const outdated = require("./outdated");
+
+function start() {
+  perf.start("apiCall");
+
+  const configFile = path.resolve(cwd, "epu-config.json");
+  let config;
+  if (!fs.existsSync(configFile)) {
+    console.log("Config File not found");
+  } else {
+    const contents = fs.readFileSync(configFile, "utf-8");
+    config = JSON.parse(contents);
+  }
+  outdated(config);
+}
+
+start();
--- outdated.js
+++ outdated.js
@@ -1,26 +1,55 @@
-'use strict';
+"use strict";
 
-const npmCheck = require('npm-check');
-var log4js = require('log4js');
-var logger = log4js.getLogger();
+const npmCheck = require("npm-check");
+const log4js = require("log4js");
+const logger = log4js.getLogger();
+const perf = require("execution-time")();
+logger.level = "debug";
 
-function checkDependencies() {
-  return npmCheck()
-  .then((currentState) => {
-    const outdatedPackage = currentState.get('packages').find(pkg => {
-      const { moduleName, mismatch } = pkg;
-      return mismatch === true;
-    });
-    if (outdatedPackage) {
-      const { moduleName, latest } = outdatedPackage;
-      logger.error(`Please update all your packages, ${moduleName} needs to be updated to ${latest}!! \n`);
-      return process.exit(1);
-    }
-    return process.exit(0);
-  })
-  .catch((error) => {
-    logger.error(error);
-    process.exit(1);
-  });
+function exit(status) {
+  const results = perf.stop("apiCall");
+  console.log(`It took ${results.preciseWords}`);
+  return process.exit(status);
 }
-checkDependencies();
+
+function checkDependencies(config = {}) {
+  const { ignore = "", checkModuleswithName = "" } = config;
+  return npmCheck()
+    .then(currentState => {
+      const outdatedPackages = currentState.get("packages").filter(pkg => {
+        const { bump, moduleName } = pkg;
+        const isIgnored = [].concat(ignore).some(semver => semver === bump);
+        const ignoreModule = !moduleName.includes(checkModuleswithName);
+        if (ignoreModule) {
+          logger.info(
+            `Ignoring Module '${moduleName}' because it doesn't contain ${checkModuleswithName} in module name`
+          );
+          return null;
+        }
+        if (isIgnored) {
+          return null;
+        }
+        if (bump) {
+          return pkg;
+        }
+        return bump;
+      });
+      if (outdatedPackages) {
+        let outdatedPackagesLog = `Please update all your packages, the following packages are out of date \n`;
+        outdatedPackages.forEach(outdatedPackage => {
+          const { moduleName, latest } = outdatedPackage;
+          outdatedPackagesLog += `'${moduleName}' needs to be updated to ${latest}!! \n`;
+        });
+        logger.error(outdatedPackagesLog);
+        return exit(1);
+      }
+      logger.info(`Your packages are up to date!!! \n`);
+      return exit(0);
+    })
+    .catch(error => {
+      logger.error(error);
+      return exit(1);
+    });
+}
+
+module.exports = checkDependencies;
